feddb125b31fc286902c90867dc1baad9aff109e
zephyr <zephyr@noreply.mail.com>
2021-01-03 18:41:03 +0300

    [add] command line arguments

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+int main(int argc, char **argv)
 {
     printf("running...\n");
 
+    int i;
+    for(i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    printf("done");
     return 0;
 }
