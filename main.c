65c1678d0395161a3bb81541a689da63e04014a8
zephyr <zephyr@noreply.mail.com>
2021-01-03 23:59:53 +0300

    [add] read file - full header

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 
 void readFile(char fileName[])
 {
-    printf("read file: %s\n", fileName);
-
     FILE *file;
     file = fopen(fileName, "rb");
     if (file == NULL)
@@ -12,26 +10,71 @@ void readFile(char fileName[])
         return;
     }
 
-    printf("offset 0x100 - rom header\n");
     fseek(file, 0x100, SEEK_SET);
 
-    char systemType[16];
-    fread(&systemType, 16, 1, file);
-    systemType[16] = '\0';
+    unsigned char systemType[16];
+    fread(systemType, sizeof(systemType), 1, file);
 
-    printf("system type: %s\n", systemType);
+    unsigned char copyrightAndReleaseDate[16];
+    fread(copyrightAndReleaseDate, sizeof(copyrightAndReleaseDate), 1, file);
+
+    unsigned char gameTitleDomestic[48];
+    fread(gameTitleDomestic, sizeof(gameTitleDomestic), 1, file);
+
+    unsigned char gameTitleOverseas[48];
+    fread(gameTitleOverseas, sizeof(gameTitleOverseas), 1, file);
+
+    unsigned char serialNumber[14];
+    fread(serialNumber, sizeof(serialNumber), 1, file);
+
+    unsigned char romChecksum[2];
+    fread(romChecksum, sizeof(romChecksum), 1, file);
+
+    unsigned char deviceSupport[16];
+    fread(deviceSupport, sizeof(deviceSupport), 1, file);
+
+    unsigned char romAddressRange[8];
+    fread(romAddressRange, sizeof(romAddressRange), 1, file);
+
+    unsigned char ramAddressRange[8];
+    fread(ramAddressRange, sizeof(ramAddressRange), 1, file);
+
+    unsigned char extraMemory[12];
+    fread(extraMemory, sizeof(extraMemory), 1, file);
+
+    unsigned char modemSupport[12];
+    fread(modemSupport, sizeof(modemSupport), 1, file);
+
+    unsigned char reservedFirst[40];
+    fread(reservedFirst, sizeof(reservedFirst), 1, file);
+
+    unsigned char regionSupport[3];
+    fread(regionSupport, sizeof(regionSupport), 1, file);
+
+    unsigned char reservedSecond[13];
+    fread(reservedSecond, sizeof(reservedSecond), 1, file);
 
     fclose(file);
+
+    printf("system type               : %.*s\n", (int) sizeof(systemType), systemType);
+    printf("copyright and release date: %.*s\n", (int) sizeof(copyrightAndReleaseDate), copyrightAndReleaseDate);
+    printf("game title (domestic)     : %.*s\n", (int) sizeof(gameTitleDomestic), gameTitleDomestic);
+    printf("game title (overseas)     : %.*s\n", (int) sizeof(gameTitleOverseas), gameTitleOverseas);
+    printf("serial number             : %.*s\n", (int) sizeof(serialNumber), serialNumber);
+    printf("rom checksum              : %#X %#X\n", romChecksum[0], romChecksum[1]);
+    printf("device support            : %.*s\n", (int) sizeof(deviceSupport), deviceSupport);
+    printf("rom address range         : %.*s\n", (int) sizeof(romAddressRange), romAddressRange);
+    printf("ram address range         : %.*s\n", (int) sizeof(ramAddressRange), ramAddressRange);
+    printf("extra memory              : %.*s\n", (int) sizeof(extraMemory), extraMemory);
+    printf("modem support             : %.*s\n", (int) sizeof(modemSupport), modemSupport);
+    printf("reserved                  : %.*s\n", (int) sizeof(reservedFirst), reservedFirst);
+    printf("region support            : %.*s\n", (int) sizeof(regionSupport), regionSupport);
+    printf("reserved                  : %.*s\n", (int) sizeof(reservedSecond), reservedSecond);
 }
 
 int main(int argc, char **argv)
 {
-    printf("mega drive rom header\n");
-
-    int i;
-    for(i = 0; i < argc; i++) {
-        printf("argv[%d]: %s\n", i, argv[i]);
-    }
+    printf("mega drive rom header\n\n");
 
     readFile(argv[1]);
 
